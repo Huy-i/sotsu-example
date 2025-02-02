5a3c62b9829656da6bebe57bf4881302b3c1fe73
Hui Hui <huihui@HuinoMacBook-Air.local>
2025-02-02 14:19:41 +0900

    create lang , cache:app.php

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -10,13 +10,8 @@ class Category extends Model
         'category_name',
     ];
 
-<<<<<<< HEAD
     public function questions()
     {
         return $this->hasMany(Question::class);
-=======
-    public function questions(){
-    return $this->hasMany(Question::class);
->>>>>>> e0f0a30c5462c4cae4c0388412c0664063973e35
     }
 }
---
+++ lang/kr/messages.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'welcome' => 'Welcome to our application',
+    'about' => 'About us',
+    'contact' => 'Contact us',
+    'list' => 'List of items',
+
+];
